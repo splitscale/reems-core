e25f029fcb5afa27d9998419f8701bf667c22046
jiv <jivayydelacruz@gmail.com>
2023-05-25 16:20:06 +0800

    Added History Interactor test

---
+++ src/test/java/com/splitscale/reems/Unit/Interactors/history/DeleteHistoryInteractorTest.java
@@ -0,0 +1,41 @@
+package com.splitscale.reems.Unit.Interactors.history;
+
+import static org.mockito.Mockito.verify;
+
+import com.splitscale.reems.history.delete.DeleteHistoryInteractor;
+import com.splitscale.reems.repositories.HistoryRepository;
+import java.io.IOException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class DeleteHistoryInteractorTest {
+  @Mock
+  private HistoryRepository repository;
+
+  @InjectMocks
+  private DeleteHistoryInteractor interactor;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  void deleteHistory_shouldInvokeRepositoryDeleteById() throws IOException {
+    String historyId = "123";
+
+    interactor.deleteHistory(historyId);
+
+    verify(repository).deleteById(historyId);
+  }
+
+  @Test
+  void deleteAllHistory_shouldInvokeRepositoryDeleteAll() throws IOException {
+    interactor.deleteAllHistory();
+
+    verify(repository).deleteAll();
+  }
+}
---
+++ src/test/java/com/splitscale/reems/Unit/Interactors/history/EditHistoryInteractorTest.java
@@ -0,0 +1,35 @@
+package com.splitscale.reems.Unit.Interactors.history;
+
+import static org.mockito.Mockito.verify;
+
+import com.splitscale.reems.history.History;
+import com.splitscale.reems.history.edit.EditHistoryInteractor;
+import com.splitscale.reems.repositories.HistoryRepository;
+import java.io.IOException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class EditHistoryInteractorTest {
+  @Mock
+  private HistoryRepository repository;
+
+  @InjectMocks
+  private EditHistoryInteractor interactor;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  void editHistory_shouldInvokeRepositoryUpdate() throws IOException {
+    History history = new History(null, null, null, null); // Create a sample History object for testing
+
+    interactor.editHistory(history);
+
+    verify(repository).update(history);
+  }
+}
