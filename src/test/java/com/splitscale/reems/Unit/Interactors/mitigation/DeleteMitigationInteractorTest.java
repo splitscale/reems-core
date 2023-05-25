4cc575b91f031b86dd5bc5b8a9e23dd3a97e91d9
jiv <jivayydelacruz@gmail.com>
2023-05-25 16:31:03 +0800

    Added MItigation Interactor test

---
+++ src/test/java/com/splitscale/reems/Unit/Interactors/mitigation/DeleteMitigationInteractorTest.java
@@ -0,0 +1,34 @@
+package com.splitscale.reems.Unit.Interactors.mitigation;
+
+import static org.mockito.Mockito.verify;
+
+import com.splitscale.reems.mitigation.delete.DeleteMitigationInteractor;
+import com.splitscale.reems.repositories.MitigationRepository;
+import java.io.IOException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class DeleteMitigationInteractorTest {
+  @Mock
+  private MitigationRepository repository;
+
+  @InjectMocks
+  private DeleteMitigationInteractor interactor;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  void deleteMitigation_shouldInvokeRepositoryDelete() throws IOException {
+    String mitigationId = "123";
+
+    interactor.deleteMitigation(mitigationId);
+
+    verify(repository).delete(mitigationId);
+  }
+}
---
+++ src/test/java/com/splitscale/reems/Unit/Interactors/mitigation/EditMitigationInteractorTest.java
@@ -0,0 +1,43 @@
+package com.splitscale.reems.Unit.Interactors.mitigation;
+
+import static org.mockito.Mockito.verify;
+
+import com.splitscale.reems.mitigation.Mitigation;
+import com.splitscale.reems.mitigation.edit.EditMitigationInteractor;
+import com.splitscale.reems.repositories.MitigationRepository;
+import java.io.IOException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class EditMitigationInteractorTest {
+  @Mock
+  private MitigationRepository repository;
+
+  @InjectMocks
+  private EditMitigationInteractor interactor;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  void editMitigation_shouldInvokeRepositoryUpdate() throws IOException {
+    Mitigation mitigation = new Mitigation(
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ); // Create a sample Mitigation object for testing
+
+    interactor.editMitigation(mitigation);
+
+    verify(repository).update(mitigation);
+  }
+}
