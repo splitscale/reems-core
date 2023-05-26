3db252b521f1be8686bf538bf9eb7d7a09fd42d0
Yui187 <steven.ballaret-17@cpu.edu.ph>
2023-05-26 22:22:04 +0800

    delete wrong service

--- src/main/java/com/splitscale/reems/services/ShieldService.java
+++
@@ -1,8 +0,0 @@
-package com.splitscale.reems.services;
-
-public interface ShieldService<T, J> {
-  void register(T request);
-  T login(T request);
-  J validate(String token, String validationData);
-  String invalidate(String token);
-}
