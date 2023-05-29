2b6405bcfe623f27d5ced105dde575743deab213
Yui187 <steven.ballaret-17@cpu.edu.ph>
2023-05-30 01:48:04 +0800

    tenantandproperty

--- src/main/java/com/splitscale/reems/core/workflow/TenantAndProperty.java
+++ src/main/java/com/splitscale/reems/core/workflow/TenantAndProperty.java
@@ -5,29 +5,55 @@ public class TenantAndProperty {
     private String tenantName;
     private String propertyId;
     private String propertyName;
-    
+    private String location;
+    private String status;
+
     public String getTenantId() {
         return tenantId;
     }
+
     public void setTenantId(String tenantId) {
         this.tenantId = tenantId;
     }
+
     public String getTenantName() {
         return tenantName;
     }
+
     public void setTenantName(String tenantName) {
         this.tenantName = tenantName;
     }
+
     public String getPropertyId() {
         return propertyId;
     }
+
     public void setPropertyId(String propertyId) {
         this.propertyId = propertyId;
     }
+
     public String getPropertyName() {
         return propertyName;
     }
+
     public void setPropertyName(String propertyName) {
         this.propertyName = propertyName;
     }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }
