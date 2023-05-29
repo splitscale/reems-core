ba39db983e9c7b9f1290f2f9e68638a955b30c4a
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-05-29 11:21:00 +0800

    add create history
    might break dabaseimpl

--- src/main/java/com/splitscale/reems/repositories/HistoryRepository.java
+++ src/main/java/com/splitscale/reems/repositories/HistoryRepository.java
@@ -1,10 +1,14 @@
 package com.splitscale.reems.repositories;
 
 import com.splitscale.reems.history.History;
+import com.splitscale.reems.history.HistoryRequest;
+
 import java.io.IOException;
 import java.util.List;
 
 public interface HistoryRepository {
+  public String create(HistoryRequest request) throws IOException;
+
   public List<History> getAll() throws IOException;
 
   public History getById(String id) throws IOException;
