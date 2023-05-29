9355d2beeb11d8a2913964d2743f18fd1919d6c6
jiv <jivayydelacruz@gmail.com>
2023-05-29 11:46:37 +0800

    Fixed Expense Interactor

--- src/test/java/com/splitscale/reems/Unit/Entities/ExpenseTest.java
+++ src/test/java/com/splitscale/reems/Unit/Entities/ExpenseTest.java
@@ -1,44 +1,56 @@
 package com.splitscale.reems.Unit.Entities;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 import com.splitscale.reems.expenses.Expense;
+
 import java.util.Date;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExpenseTest {
 
   @Test
-  public void testGettersAndSetters() {
-    // Create a sample Expense instance
-    String id = "01";
-    String expense = "4221";
+  public void testExpenseProperties() {
+    // Arrange
+    String id = "123";
     Date created = new Date();
     Date modified = new Date();
+    String value = "10.5";
+    String unit = "USD";
 
-    Expense expenseObj = new Expense(id, expense, created, modified);
+    // Act
+    Expense expense = new Expense(id, created, modified, value, unit);
 
-    // Test the getters
-    assertEquals(id, expenseObj.getId());
-    assertEquals(expense, expenseObj.getExpense());
-    assertEquals(created, expenseObj.getCreated());
-    assertEquals(modified, expenseObj.getModified());
+    // Assert
+    assertEquals(id, expense.getId());
+    assertEquals(created, expense.getCreated());
+    assertEquals(modified, expense.getModified());
+    assertEquals(value, expense.getValue());
+    assertEquals(unit, expense.getUnit());
+  }
 
-    // Test the setters
-    String newId = "02";
-    String newExpense = "3563";
-    Date newCreated = new Date();
-    Date newModified = new Date();
+  @Test
+  public void testExpenseSetters() {
+    // Arrange
+    Expense expense = new Expense();
+    String id = "456";
+    Date created = new Date();
+    Date modified = new Date();
+    String value = "15.2";
+    String unit = "EUR";
 
-    expenseObj.setId(newId);
-    expenseObj.setExpense(newExpense);
-    expenseObj.setCreated(newCreated);
-    expenseObj.setModified(newModified);
+    // Act
+    expense.setId(id);
+    expense.setCreated(created);
+    expense.setModified(modified);
+    expense.setValue(value);
+    expense.setUnit(unit);
 
-    // Test the updated values
-    assertEquals(newId, expenseObj.getId());
-    assertEquals(newExpense, expenseObj.getExpense());
-    assertEquals(newCreated, expenseObj.getCreated());
-    assertEquals(newModified, expenseObj.getModified());
+    // Assert
+    assertEquals(id, expense.getId());
+    assertEquals(created, expense.getCreated());
+    assertEquals(modified, expense.getModified());
+    assertEquals(value, expense.getValue());
+    assertEquals(unit, expense.getUnit());
   }
 }
